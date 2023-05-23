4b51874d31ec13eb8d90d0dd20690c0c86d09c45
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2023-05-23 18:24:20 +0200

    feat: add fetching sample books from api

--- src/Library.jsx
+++ src/Library.jsx
@@ -1,9 +1,31 @@
-const library = [
-  { title: 'A Game of Thrones ', author: 'George R. R. Martin' },
-  { title: 'A Clash of Kings ', author: 'George R. R. Martin' },
-];
+import { useEffect, useState } from 'react';
+
+// const library = [
+//   { title: 'A Game of Thrones ', author: 'George R. R. Martin' },
+//   { title: 'A Clash of Kings ', author: 'George R. R. Martin' },
+// ];
 
 export default function Library() {
+  const [library, setLibrary] = useState([]);
+
+  async function requestBooks() {
+    const response = await fetch(
+      'https://openlibrary.org/search.json?q=first_publish_year:2023&sort=new&limit=5&language=eng'
+    );
+    const json = await response.json();
+    setLibrary(
+      json.docs.map((entry) => ({
+        key: entry.key,
+        title: entry.title,
+        author: entry.author_name[0],
+      }))
+    );
+  }
+
+  useEffect(() => {
+    requestBooks();
+  }, []);
+
   return (
     <ul className="library">
       {library.map((book) => (
